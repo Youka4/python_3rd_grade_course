f9750addf7e6cd10298d6b586bc60b5465752eda
Youka4 <dim.youka4@gmail.com>
2025-03-09 20:02:56 +0300

    task 1

---
+++ hw_1/task_1.py
@@ -0,0 +1,20 @@
+import sys
+
+def number_lines(file):
+    for i, line in enumerate(file, start=1):
+        print(f"{i}\t{line}", end="")
+
+def main():
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        try:
+            with open(filename, "r", encoding="utf-8") as file:
+                number_lines(file)
+        except FileNotFoundError:
+            print(f"nl(python script): {filename}: No such file or directory", file=sys.stderr)
+            sys.exit(1)
+    else:
+        number_lines(sys.stdin)
+
+if __name__ == "__main__":
+    main()
